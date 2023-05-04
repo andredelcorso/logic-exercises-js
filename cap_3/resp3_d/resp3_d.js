520e2ec98284b26892d5cb36feb5f613543ba38c
André Del Corso <2101228@aluno.univesp.br>
2023-05-04 00:43:00 -0300

    Exercises
    adding new exercises

---
+++ cap_3/resp3_d/resp3_d.js
@@ -0,0 +1,23 @@
+function Calcular() { 
+    const sideOne = window.document.getElementById('sideOne')
+    const sideTwo = window.document.getElementById('sideTwo')
+    const sideThree = window.document.getElementById('sideThree')
+
+    const ladoUm = Number(sideOne.value)
+    const ladoDois = Number(sideTwo.value)
+    const ladoTres = Number(sideThree.value)
+
+    if (ladoUm == ladoDois && ladoDois == ladoTres && ladoTres == ladoUm) { 
+        const resposta = window.document.getElementById('resposta1').textContent = (`Lados Podem Formar um Triângulo`)
+        const resposta2 = window.document.getElementById('resposta2').textContent = (`Tipo: Equilatero`)
+    } else if (ladoUm !== ladoDois && ladoDois == ladoTres && ladoTres == ladoUm || ladoUm == ladoDois && ladoDois !== ladoTres && ladoDois == ladoUm || ladoUm == ladoDois && ladoDois == ladoTres && ladoTres !== ladoUm) { 
+        const resposta = window.document.getElementById('resposta1').textContent = (`Lados Podem Formar um Triângulo`)
+        const resposta2 = window.document.getElementById('resposta2').textContent = (`Tipo: Isósceles`)
+    } else if (ladoUm !== ladoDois !== ladoTres !== ladoUm) {
+        const resposta = window.document.getElementById('resposta1').textContent = (`Lados Podem Formar um Triângulo`)
+        const resposta2 = window.document.getElementById('resposta2').textContent = (`Tipo: Escaleno`)
+    }
+}
+
+    const btnVerify = window.document.getElementById('verifyButton')
+    btnVerify.addEventListener('click', Calcular)
