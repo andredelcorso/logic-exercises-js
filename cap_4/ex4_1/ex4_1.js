9411a94df2815ff39bea561b264ec79aa111863d
André Del Corso <2101228@aluno.univesp.br>
2023-05-04 02:28:45 -0300

    Exercises
    adding new exercises

---
+++ cap_4/ex4_1/ex4_1.js
@@ -0,0 +1,19 @@
+function Tabuada () {
+    let num = window.document.getElementById('numero')
+    let number = Number(num.value)
+    let tabuadaString = ("")
+
+    for (let tabuada = 1; tabuada <= 10; tabuada++) {
+
+        tabuadaString += `${number} x ${tabuada} = ${number * tabuada}`
+        
+
+    }   
+
+   
+    window.document.getElementById('resposta').textContent = tabuadaString 
+}
+
+
+    const calcTabuada = window.document.getElementById('calcTabuada')
+    calcTabuada.addEventListener('click', Tabuada)
