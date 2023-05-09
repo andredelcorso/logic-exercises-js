abcc3c291146d89fb53f78862942c9ad60b0560c
André Del Corso <2101228@aluno.univesp.br>
2023-05-08 23:44:23 -0300

    Exercises
    editing exercises

--- cap_4/resp4_b/resp4_b.js
+++ cap_4/resp4_b/resp4_b.js
@@ -1,32 +1,23 @@
-let numChinchilas = 0  
-let numAnos = 0
+function Calcular() {
+  let quantasChinchilas = document.getElementById('QtdChinchilas');
+  let quantosAnos = document.getElementById('QtdAnos');
 
-let resposta = ""
+  let Chinchilas = Number(quantasChinchilas.value);
+  let Anos = Number(quantosAnos.value);
 
-function Calcular () { 
+  if (Chinchilas == 0 || Chinchilas < 2 || isNaN(Chinchilas) || Anos == 0 || isNaN(Anos)) {
+    alert('Favor, verificar os dados. Obs.: O número inicial de Chinchilas deve ser igual ou superior a 02');
+    return; // Exit the function if the data is invalid
+  }
 
-    let quantasChinchilas = document.getElementById('QtdChinchilas')
-    let quantosAnos = document.getElementById('QtdAnos')
-   
+    resposta = "" // Reset the resposta variable
 
-    let Chinchilas = Number(quantasChinchilas.value)
-    let Anos = Number(quantosAnos.value)
-
-    if (Chinchilas == 0 || Chinchilas < 2 || isNaN(Chinchilas) || Anos == 0 || isNaN(Anos)) {
-        alert('Favor, verificar os dados. Obs.: O número inicial de Chinchilas deve ser igual ou superior a 02')
+    for (let contador = 1; contador <= Anos; contador++) {    
+            resposta += contador + "º Ano: " + Chinchilas + " Chinchilas\n"
+            Chinchilas = Chinchilas * 3
     }
 
-    let reprodução = numChinchilas * 3 
-    numAnos++
-    numChinchilas + numChinchilas
-
-    for (let contador = 1; contador <= Anos; contador ++) {
-        totalAnos =+ reprodução
-        let resposta = document.getElementById('resposta').textContent = (`${numAnos}º Ano: ${reprodução} Chinchilas`)
-    
-    }
-    
-    
+    document.getElementById('resposta').textContent = resposta
 }
 
     let btnVerify = document.getElementById('verifyButton')
