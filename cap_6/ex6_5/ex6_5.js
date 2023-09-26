b9306aad4e0b69fd62d98d2974d09afda1baa8e9
André Del Corso <2101228@aluno.univesp.br>
2023-09-26 01:57:26 -0300

    ex6_5

---
+++ cap_6/ex6_5/ex6_5.js
@@ -0,0 +1,28 @@
+function generateEmail () {
+
+    let empresa = prompt('Qual o nome da empresa?')
+    let name = window.document.getElementById('employeeTxt')
+    let nome = name.value 
+    let resposta = window.document.getElementById('resposta')
+    let divide = nome.split(" ")
+
+    if (nome == "" || nome.indexOf(" ") == -1) { 
+        alert('Favor, verifique novamente e insira os dados corretamente...')
+        nome.focus()
+        return
+    }
+
+    let email = ""
+
+    let tamanho = divide.length
+
+    for (contador = 0; contador < tamanho; contador ++) { 
+        email += divide[contador].charAt(0)
+    }
+
+    email += divide[tamanho-1] + "@"+empresa+".com.br"
+
+    resposta.textContent = "E-mail: " + email.toLowerCase()
+
+
+}
