9602ce0d9f5dc39c990a290db7127eb02ab206f1
André Del Corso <2101228@aluno.univesp.br>
2023-09-27 01:35:21 -0300

    update ex6_6

--- cap_6/ex6_6/ex6_6.js
+++ cap_6/ex6_6/ex6_6.js
@@ -5,33 +5,32 @@ function passwordCheck () {
 
     let resposta = document.getElementById('resposta')
 
-
     let erros = []
 
     if (password.length < 8 || password.length > 15) { 
-        erros.push("Possuir entre 8 e 15 caracteres")
+        erros.push("deve possuir entre 8 e 15 caracteres e também: ")
     }
 
     if (password.match(/[0-9]/g) == null) { 
-        erros.push('Possuir números entre 0 e 9 (pelo menos um)!')
+        erros.push('\n - deve possuir números entre 0 e 9 (pelo menos um);')
     }
 
-    if (!password.match(/[A-Z]/g) || password.match(/[A-Z]/g) == 1) { 
-        erros.push('Deve possuir pelo menos DUAS letras maiúsculas.')
+    if (!password.match(/[A-Z]/g) || password.match(/[A-Z]/g).length == 1) { 
+        erros.push('\n - pelo menos DUAS letras maiúsculas;')
     }
 
     if (!password.match(/[a-z]/g)) { 
-        erros.push('Deve possuir pelo menos uma letra minúscula.')
+        erros.push('\n - pelo menos uma letra minúscula;')
     }
 
     if (!password.match(/[\W|_]/g)) { 
-        erros.push('Deve possuir pelo menos 1 símbolo!')
+        erros.push('\n - pelo menos 1 símbolo!')
     }
 
     if (erros.length == 0) { 
         resposta.textContent = "Ok, senha válida."
     } else { 
-        resposta.TextContent = "Problema encontrado. Refaça sua senha." + erros.join(" ,")
+        resposta.textContent = "Problema encontrado. Sua senha " + erros.join("")
     }
 
 }
