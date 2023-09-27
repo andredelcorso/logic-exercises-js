0f1d777983cd8c574bb56e0b38f2e18d6b397d32
André Del Corso <2101228@aluno.univesp.br>
2023-09-27 02:06:11 -0300

    update ex6_7

--- cap_6/ex6_7/ex6_7.js
+++ cap_6/ex6_7/ex6_7.js
@@ -1,6 +1,18 @@
 function change () { 
+
     let texto = document.getElementById('txtBox')
     let txt = texto.value 
 
-    
+    let newStr = txt.replace(/A/g, "X").toLowerCase()
+
+
+    let resposta = document.getElementById('resposta').textContent = newStr
+
+}
+
+function showF () { 
+    let texto = document.getElementById('txtBox')
+    let txt = texto.value 
+
+    let resposta = document.getElementById('resposta').textContent = txt 
 }
